1f31556909edd5ea8509a30ff324e734bc8283be
Christian Tannahill <cstannahill10@gmail.com>
2025-07-09 15:06:32 -0500

    Lower aim sensitivity and show player id in card selection

--- src/systems/card_selection.rs
+++ src/systems/card_selection.rs
@@ -21,6 +21,7 @@ pub fn setup_card_ui(mut commands: Commands, selection: Res<CardSelection>) {
                 style: Style {
                     width: Val::Percent(100.0),
                     height: Val::Percent(100.0),
+                    flex_direction: FlexDirection::Column,
                     justify_content: JustifyContent::SpaceEvenly,
                     align_items: AlignItems::Center,
                     ..default()
@@ -32,39 +33,78 @@ pub fn setup_card_ui(mut commands: Commands, selection: Res<CardSelection>) {
         ))
         .id();
 
-    for (i, card) in selection.choices.iter().enumerate() {
-        commands.entity(root).with_children(|parent| {
-            parent
-                .spawn((
-                    ButtonBundle {
-                        style: Style {
-                            width: Val::Px(180.0),
-                            height: Val::Px(120.0),
-                            flex_direction: FlexDirection::Column,
-                            justify_content: JustifyContent::Center,
-                            align_items: AlignItems::Center,
-                            margin: UiRect::all(Val::Px(5.0)),
-                            ..default()
-                        },
-                        background_color: Color::DARK_GRAY.into(),
+    // Display which player is selecting cards
+    let player_id = selection.loser.unwrap_or(0);
+    commands.entity(root).with_children(|parent| {
+        parent
+            .spawn(NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                background_color: Color::NONE.into(),
+                ..default()
+            })
+            .with_children(|p| {
+                p.spawn(TextBundle::from_section(
+                    format!("Player {player_id} - choose a card"),
+                    TextStyle {
+                        font_size: 32.0,
+                        color: Color::WHITE,
                         ..default()
                     },
-                    CardButton { index: i },
-                ))
-                .with_children(|p| {
-                    p.spawn(TextBundle::from_sections([
-                        TextSection::new(
-                            card.name,
-                            TextStyle { font_size: 24.0, color: Color::WHITE, ..default() },
-                        ),
-                        TextSection::new(
-                            format!("\n{}", card.description),
-                            TextStyle { font_size: 16.0, color: Color::WHITE, ..default() },
-                        ),
-                    ]));
-                });
-        });
-    }
+                ));
+            });
+    });
+
+    commands.entity(root).with_children(|parent| {
+        parent
+            .spawn(NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    justify_content: JustifyContent::SpaceEvenly,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                background_color: Color::NONE.into(),
+                ..default()
+            })
+            .with_children(|row| {
+                for (i, card) in selection.choices.iter().enumerate() {
+                    row
+                        .spawn((
+                            ButtonBundle {
+                                style: Style {
+                                    width: Val::Px(180.0),
+                                    height: Val::Px(120.0),
+                                    flex_direction: FlexDirection::Column,
+                                    justify_content: JustifyContent::Center,
+                                    align_items: AlignItems::Center,
+                                    margin: UiRect::all(Val::Px(5.0)),
+                                    ..default()
+                                },
+                                background_color: Color::DARK_GRAY.into(),
+                                ..default()
+                            },
+                            CardButton { index: i },
+                        ))
+                        .with_children(|p| {
+                            p.spawn(TextBundle::from_sections([
+                                TextSection::new(
+                                    card.name,
+                                    TextStyle { font_size: 24.0, color: Color::WHITE, ..default() },
+                                ),
+                                TextSection::new(
+                                    format!("\n{}", card.description),
+                                    TextStyle { font_size: 16.0, color: Color::WHITE, ..default() },
+                                ),
+                            ]));
+                        });
+                }
+            });
+    });
 }
 
 pub fn cleanup_card_ui(mut commands: Commands, query: Query<Entity, With<CardUiRoot>>) {
